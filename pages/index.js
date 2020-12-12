5e2295b5ff5c7d27cff07ee801e3deca4f8c5c75
Maik Marschner <an@maik.tk>
2020-12-13 00:47:13 +0100

    Remove the footer.

--- pages/index.js
+++ pages/index.js
@@ -39,16 +39,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          &copy; ChunkyCloud and its contributors
-        </a>
-      </footer>
     </div>
   );
 }
